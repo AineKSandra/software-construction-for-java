156aad15f179b46d4efa55cca92ccaf0df6470ac
AineKSandra <ainesandra8@gmail.com>
2023-03-30 06:12:47 -0700

    Java code on shapes and their properties

---
+++ Posn.java
@@ -0,0 +1,20 @@
+
+public class Posn {
+        //instance variables
+        private int x;
+        private int y;
+    
+        //constructor
+        public Posn(int x,int y){
+            this.x = x;
+            this.y = y;
+        }
+        //getters
+        public int getX(){
+            return x;
+        }
+        public int getY(){
+            return y;
+        }
+    }
+
---
+++ Square.java
@@ -0,0 +1,8 @@
+public class Square extends Rectangle {
+    public Square (Posn center, double side){
+        super(center, side, side);
+    }
+    public double getSide(){
+        return getWidth();
+    }
+}
